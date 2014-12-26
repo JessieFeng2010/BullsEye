f0a9d15a9ba4ae01efe33fc87ea60189241d601c
Jessie <jessie_feng2010@hotmail.com>
2014-12-26 11:49:57 +0800

    smarten up

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -23,6 +23,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         //startNewRound()
+        
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighLighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageNormal, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        let trackLeftImage = UIImage(named: "SliderTrackLeft")
+        let trackLeftResizable = trackLeftImage?.resizableImageWithCapInsets(insets)
+        slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        
+        let trackRightImage = UIImage(named: "SliderTrackRight")
+        let trackRightResizable = trackRightImage?.resizableImageWithCapInsets(insets)
+        slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        
         startNewGame()
         updateLabels()
     }
