a24c9e4370707d517eb37bbea92a44dc0caeec34
Jessie <jessie_feng2010@hotmail.com>
2015-01-08 09:30:27 +0800

    add animation when startNewGame

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
@@ -90,6 +91,12 @@ class ViewController: UIViewController {
     @IBAction func startOver() {
         startNewGame()
         updateLabels()
+        
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
     }
     
     func startNewGame() {
